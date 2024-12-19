dd6109af7ec52b2e24953427961a01b97f7f4cf4
Petr <8441404@gmail.com>
2024-12-19 16:19:25 +0300

    HW_04_prepare

--- persist/src/test/java/ru/javaops/masterjava/persist/DBITestProvider.java
+++ persist/src/test/java/ru/javaops/masterjava/persist/DBITestProvider.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 
 public class DBITestProvider {
     public static void initDBI() {
-        initDBI("jdbc:postgresql://localhost:5432/masterjava", "user", "password");
+        initDBI("jdbc:postgresql://petrtitov.com:35432/postgres", "admin", "Karamba1987");
     }
 
     public static void initDBI(String dbUrl, String dbUser, String dbPassword) {
