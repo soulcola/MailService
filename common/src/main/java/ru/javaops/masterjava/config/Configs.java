9a8aad7e1e828156aef7d886779157cf384a94bf
Petr <8441404@gmail.com>
2025-01-10 11:42:08 +0300

    profiles impl fix

--- common/src/main/java/ru/javaops/masterjava/config/Configs.java
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -2,14 +2,27 @@ package ru.javaops.masterjava.config;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * gkislin
  * 01.11.2016
  */
+@Slf4j
 public class Configs {
 
     public static Config getConfig(String resource) {
+        String profile = System.getProperty("profile", System.getenv("profile"));
+        String[] fileNameSplit = resource.split("\\.");
+        if (profile != null) {
+            resource = profile + "-" + profile;
+            if (fileNameSplit.length > 1) {
+                resource = fileNameSplit[0] + "-" + profile + "." + fileNameSplit[1];
+            }
+            log.info("Profile: {}. Loading config file: {}", profile, resource);
+        } else {
+            log.warn("Profile not set. Loading default config file");
+        }
         return ConfigFactory.parseResources(resource).resolve();
     }
 
