07ed0e3f0835026da83d527105ef7b226f63729c
petr <8441404@gmail.com>
2025-01-14 11:14:15 +0300

    7_3_HW6_ProjectGroupProcessor

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/GroupDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/GroupDao.java
@@ -4,10 +4,12 @@ import one.util.streamex.StreamEx;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlBatch;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import ru.javaops.masterjava.persist.model.Group;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -33,4 +35,7 @@ public interface GroupDao extends AbstractDao {
         int id = insertGeneratedId(groups);
         groups.setId(id);
     }
+
+    @SqlBatch("INSERT INTO groups (name, type, project_id) VALUES (:name, CAST(:type AS group_type), :projectId)")
+    void insertBatch(@BindBean Collection<Group> groups);
 }
