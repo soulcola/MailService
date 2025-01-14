fb978c18570c24ca6bcf5a2759dd1b89619705f5
Petr <8441404@gmail.com>
2025-01-14 15:26:49 +0300

    Dependencies update SLF4J update

--- persist/src/main/java/ru/javaops/masterjava/persist/model/Group.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/model/Group.java
@@ -1,6 +1,5 @@
 package ru.javaops.masterjava.persist.model;
 
-//import com.bertoncelj.jdbi.entitymapper.Column;
 import lombok.*;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
 import ru.javaops.masterjava.persist.model.type.GroupType;
@@ -14,5 +13,5 @@ public class Group extends BaseEntity {
 
     @NonNull private String name;
     @NonNull private GroupType type;
-    @NonNull @ColumnName("project_id") private int projectId;
+    @ColumnName("project_id") private int projectId;
 }
