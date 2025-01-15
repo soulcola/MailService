0d263989183eafac81c4fae8a7c47d2d5811ab58
Petr <8441404@gmail.com>
2025-01-15 15:19:07 +0300

    7_9_refactoring_send_to_group

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailService.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailService.java
@@ -3,7 +3,7 @@ package ru.javaops.masterjava.service.mail;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
-import java.util.List;
+import java.util.Set;
 
 @WebService(targetNamespace = "http://mail.javaops.ru/")
 //@SOAPBinding(
@@ -13,9 +13,9 @@ import java.util.List;
 public interface MailService {
 
     @WebMethod
-    void sendMail(
-            @WebParam(name = "to") List<Addressee> to,
-            @WebParam(name = "cc") List<Addressee> cc,
+    String sendToGroup(
+            @WebParam(name = "to") Set<Addressee> to,
+            @WebParam(name = "cc") Set<Addressee> cc,
             @WebParam(name = "subject") String subject,
             @WebParam(name = "body") String body);
 }
