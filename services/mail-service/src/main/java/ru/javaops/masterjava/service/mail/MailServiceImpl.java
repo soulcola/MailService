a63ccafeedecceb7701b3301e00cc4293e6c9a34
Petr <8441404@gmail.com>
2025-01-20 00:30:56 +0300

    9_2_HW8_MTOM

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.javaops.masterjava.service.mail;
 
 import jakarta.jws.WebService;
+import jakarta.xml.ws.soap.MTOM;
 import ru.javaops.masterjava.web.WebStateException;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.Set;
 @WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService", targetNamespace = "http://mail.javaops.ru/"
 //          , wsdlLocation = "WEB-INF/wsdl/mailService.wsdl"
 )
+//@StreamingAttachment(parseEagerly=true, memoryThreshold=40000L)
+@MTOM
 public class MailServiceImpl implements MailService {
     public String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body, List<Attachment> attachments) throws WebStateException {
         return MailSender.sendToGroup(to, cc, subject, body);
