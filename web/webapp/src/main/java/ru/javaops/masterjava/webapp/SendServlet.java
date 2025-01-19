8601fad75872f20bc4ed1d35ea5fd0815c0d6524
Petr <8441404@gmail.com>
2025-01-20 00:39:42 +0300

    9_3_HW8_webapp_attach

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/SendServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/SendServlet.java
@@ -1,6 +1,8 @@
 package ru.javaops.masterjava.webapp;
 
+import com.google.common.collect.ImmutableList;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,11 +10,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailWSClient;
-
+import ru.javaops.masterjava.service.mail.util.Attachments;
+import jakarta.servlet.http.Part;
 import java.io.IOException;
 
 @WebServlet("/send")
 @Slf4j
+@MultipartConfig
 public class SendServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,7 +28,10 @@ public class SendServlet extends HttpServlet {
             String users = req.getParameter("users");
             String subject = req.getParameter("subject");
             String body = req.getParameter("body");
-            GroupResult groupResult = MailWSClient.sendBulk(MailWSClient.split(users), subject, body, null);
+            Part filePart = req.getPart("attach");
+            GroupResult groupResult = MailWSClient.sendBulk(MailWSClient.split(users), subject, body,
+                    filePart == null ? null :
+                            ImmutableList.of(Attachments.getAttachment(filePart.getSubmittedFileName(), filePart.getInputStream())));
             result = groupResult.toString();
             log.info("Processing finished with result: {}", result);
         } catch (Exception e) {
